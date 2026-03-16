c7707c5c3fbd774d171f869a5921f11b5feed382
MotyaSS <motyazz32@gmail.com>
2026-03-16 19:06:20 +0300

    refactor: move app from myapp.py to mai.py

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
-from cache import LRUCache
+import random
+import string
+import time
 
-def main():
-    cache = LRUCache(100)
-    
-    cache.set('Jesse', 'Pinkman')
-    cache.set('Walter', 'White')
-    cache.set('Jesse', 'James')
+def app(environ, start_response):
+    print("Received request:"
+          f"\nMethod: {environ['REQUEST_METHOD']}"
+          f"\nPath: {environ['PATH_INFO']}"
+          f"\nUser Agent: {environ['HTTP_USER_AGENT']}")
 
-    print(cache.get('Jesse'))
-    
-    cache.rem('Walter')
-    
-    print(cache.get('Walter'))
-    
+    length = random.randint(8, 16)
+    required_symbols = "#.,!@&^%*"
 
-if __name__ == "__main__":
-    main()
+    password_chars = [
+        random.choice(string.digits),
+        random.choice(string.ascii_lowercase),
+        random.choice(string.ascii_uppercase),
+        random.choice(required_symbols),
+    ]
+
+    all_allowed_chars = string.digits + string.ascii_lowercase + string.ascii_uppercase + required_symbols
+    password_chars.extend(random.choice(all_allowed_chars) for _ in range(length - len(password_chars)))
+    random.shuffle(password_chars)
+
+    password = "".join(password_chars).encode("utf-8")
+    time.sleep(0.05)
+
+    start_response("200 OK", [
+        ("Content-Type", "text/plain"),
+        ("Content-Length", str(len(password)))
+    ])
+    return iter([password])
